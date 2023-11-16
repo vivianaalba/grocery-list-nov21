7c2c1b8c048102e51aed989da7a8396abf04497f
Viviana Alba <141361486+vivianaalba@users.noreply.github.com>
2023-11-15 20:06:00 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let groceryItems = {
+{ name: 'Apple', category: 'Fruits', price: 1.0, quantity: 6 },
+{ name: 'Banana', category: 'Fruits', price: 0.5, quantity: 10 },
+{ name: 'Carrot', category: 'Vegetables', price: 0.8, quantity: 8 },
+{ name: 'Milk', category: 'Dairy', price: 2.0, quantity: 2 },
+{ name: 'Eggs', category: 'Dairy', price: 1.5, quantity: 12 }
+}
+
+let groceryList
